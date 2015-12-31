2fb2ae2a5af7bcf291a00a5a1ffa6ff29a51a7c9
Huu Phuc Nguyen <mr.gon1505@gmail.com>
2015-12-31 09:25:35 +0700

    Initial Commit

---
+++ DemoTikiMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoTikiMap
+//
+//  Created by Nguyen Phuc on 12/31/15.
+//  Copyright © 2015 Nguyen Phuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoTikiMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoTikiMap
+//
+//  Created by Nguyen Phuc on 12/31/15.
+//  Copyright © 2015 Nguyen Phuc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
